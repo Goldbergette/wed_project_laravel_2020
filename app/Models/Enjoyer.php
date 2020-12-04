5f653878ad3ac5e79afc384adb6db296a72664d6
Sylvie <gillissen.sylvie@gmail.com>
2020-12-04 20:09:31 +0100

    Tables crées dans Voyager + models dans Laravel

---
+++ app/Models/Enjoyer.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Enjoyer extends Model
+{
+    use HasFactory;
+}
