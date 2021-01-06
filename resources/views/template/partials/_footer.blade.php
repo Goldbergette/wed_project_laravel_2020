3d84a16dd5ede77b74867c5fa0a896c999b4e342
Sylvie <gillissen.sylvie@gmail.com>
2021-01-06 11:26:56 +0100

    Mise en place des liens dans le header et footer

--- resources/views/template/partials/_footer.blade.php
+++ resources/views/template/partials/_footer.blade.php
@@ -14,7 +14,7 @@
                             <div class="single-footer-caption mb-30">
                                 <!-- logo -->
                                 <div class="footer-logo">
-                                    <a href="index.html"><img src="{{ asset('assets/img/logo/logo2_footer.png')}}" alt=""></a>
+                                    <a href="{{ route('posts.index')}}"><img src="{{ asset('assets/img/logo/logo2_footer.png')}}" alt=""></a>
                                 </div>
                             </div>
                         </div>
